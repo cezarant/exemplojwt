7895ba7d9becb59de81d8a12cfd231ba4b37355e
cezarant <cezarantsouza@gmail.com>
2020-04-10 23:44:00 -0300

    Configurando jwt

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -4,7 +4,7 @@ const config = require('config.json');
 module.exports = jwt;
 
 function jwt(){    
-	const port = config.secret || process.env.secret;
+	const port = process.env.secret || config.secret;
 	const { secret } = config;
     return expressJwt({ secret }).unless({
         path: [            
